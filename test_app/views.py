a7be0af8c08e157de898bc2cc1f41b7b93544424
Jaikishan Didwania <jaididwania@gmail.com>
2020-05-28 17:47:27 +0530

    ImageURL

--- test_app/views.py
+++ test_app/views.py
@@ -1,15 +1,53 @@
 import requests
+from urllib.parse import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+import re
 
-BASE_CRAIGSLIST_URL= 'https://jaipur.craigslist.org/search/?query={}'
-
+BASE_CRAIGSLIST_URL= 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMG_URL='https://images.craigslist.org/{}_300x300.jpg'
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
+
     search = request.POST.get('search')    #request.POST returns a dictionary
-    context={
-        'search' : search
+    models.Search.objects.create(search=search)
+
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+
+
+    final_posting = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        #Processing the Price Part
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price='N/A'
+
+        #Generating the url for the image
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMG_URL.format(post_image_id)
+
+        else:
+            post_image_url = "https://craigslist.org/images/peace.jpg"
+
+        final_posting.append((post_title, post_url, post_price, post_image_url))
+
+    context = {
+        'search': search,
+        'final_posting': final_posting,
     }
     return  render(request, 'my_app/new_search.html', context)
