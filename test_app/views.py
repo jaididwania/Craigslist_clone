e6df2c742ba0aa818bb8379e83fee478d7ba1c92
Jaikishan Didwania <jaididwania@gmail.com>
2020-05-28 14:11:15 +0530

    Requirements

--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+import requests
 
+from django.shortcuts import render
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')    #request.POST returns a dictionary
+    context={
+        'search' : search
+    }
+    return  render(request, 'my_app/new_search.html', context)
